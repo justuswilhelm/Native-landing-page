3c277ba6f8a3d8517e4fe76bf94a7ed7c480efe5
theBliz <imbrescia.s@gmail.com>
2017-08-11 00:36:18 +0200

    push new website with hidden routes

---
+++ routes/ideas.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+const request = require('superagent');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  request
+		.get("https://habitat-api.azurewebsites.net/ideas")
+		.end(function(err, resApi){
+		    if(err){
+		     console.log(err);
+		     return next(err);
+		    } else {
+		     console.log(resApi.body);
+  			 res.render('pages/ideas.ejs', { title: 'ideas', ideas: resApi.body });
+		    }
+		});
+});
+
+router.get('/:ideaId', function(req, res, next) {
+	var ideaId = req.params.ideaId;
+	var idea = {};
+	console.log('ideaId: ', ideaId);
+	console.log('req: ', req.params);
+	console.log('req.path: ', req.path);
+	request
+		.get("https://habitat-api.azurewebsites.net/ideas/"+ideaId)
+		.end(function(err, resApi){
+		    if(err){
+		     console.log(err);
+		     return next(err);
+		    } else {
+		     idea = resApi.body;
+		     console.log('idea :', idea);
+		     request
+				.get("https://habitat-api.azurewebsites.net/users/"+idea.owner)
+				.end(function(err, resApiOwner){
+				    if(err){
+				     console.log(err);
+		     		 return next(err);
+				    } else {
+				     idea.owner = resApiOwner.body;
+				     console.log('idea.owner :', idea.owner);
+		  			 res.render('pages/idea-profile.ejs', { title: 'idea', idea: idea });
+				    }
+				});
+		    }
+		});
+});
+module.exports = router;
---
+++ routes/people.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+const request = require('superagent');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	request
+		.get("https://habitat-api.azurewebsites.net/users")
+		.end(function(err, resApi){
+		    if(err){
+		     console.log(err);
+		     return next(err);
+		    } else {
+		     console.log(resApi.body);
+  			 res.render('pages/people.ejs', { title: 'people', users: resApi.body });
+		    }
+		});
+});
+
+router.get('/:userId', function(req, res, next) {
+	var userId = req.params.userId;
+	var user = {};
+	console.log('userId: ', userId);
+	console.log('req: ', req.params);
+	console.log('req.path: ', req.path);
+	request
+		.get("https://habitat-api.azurewebsites.net/users/"+userId)
+		.end(function(err, resApi){
+		    if(err){
+		     console.log(err);
+		     return next(err);
+		    } else {
+		     //console.log(resApi.body);
+		     request
+				.get("https://habitat-api.azurewebsites.net/habitats?owner="+userId)
+				.end(function(err, resApiHabitats){
+				    if(err){
+				     console.log(err);
+		     		 return next(err);
+				    } else {
+				     user = resApi.body;
+				     user.habitats = resApiHabitats.body;
+				     //console.log('user: ', user);
+				     request
+						.get("https://habitat-api.azurewebsites.net/ideas?owner="+userId)
+						.end(function(err, resApiIdeas){
+						    if(err){
+						     console.log(err);
+		     				 return next(err);
+						    } else {
+						     user.ideas = resApiIdeas.body;
+						     console.log('qui :', user);
+				  			 res.render('pages/user-profile.ejs', { title: 'people', user: user });
+						    }
+						});
+				    }
+				});
+  			 //res.render('pages/user-profile.ejs', { title: 'people', user: resApi.body });
+		    }
+		});
+});
+
+module.exports = router;
